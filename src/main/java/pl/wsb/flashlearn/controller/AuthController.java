74ad4f5d98d56fa9976df30493357d83b8366b6e
Rafał 154141 <pzx121389@student.poznan.merito.pl>
2025-01-06 14:35:07 +0100

    Recursive requests login redirection

--- src/main/java/pl/wsb/flashlearn/controller/AuthController.java
+++ src/main/java/pl/wsb/flashlearn/controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class AuthController {
 
-    @GetMapping("/")
+    @GetMapping("/**")
     public String home() {
         return "redirect:/login"; // Przekierowanie na stronę logowania
     }
